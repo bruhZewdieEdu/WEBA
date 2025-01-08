8f6d0d43f8fa1e1ccde378478e66e04c656fe477
bruhZewdieEdu <bruh.zwd@eduge.ch>
2025-01-08 17:08:10 +0100

    456

--- views/utilisateurs/index.php
+++ views/utilisateurs/index.php
@@ -1,5 +1,4 @@
 <?php if (!empty($utilisateurs)) : ?>
-<<<<<<< HEAD
     <h1>Navigation entre les utilisateurs</h1>
     <div id="userContainer">
         <!-- L'utilisateur actif sera affiché ici -->
@@ -59,28 +58,8 @@
         // Initialiser l'affichage
         updateDisplay();
     </script>
-=======
-    <h1>Liste des utilisateurs</h1>
-    <ul>
-        <?php foreach ($utilisateurs as $user) : ?>
-            <li>
-                <?= htmlspecialchars($user['UTILISATEUR_NOM']) ?>
-                <?= htmlspecialchars($user['UTILISATEUR_PRENOM']) ?>
-                (<?= htmlspecialchars($user['UTILISATEUR_MAIL']) ?>)
-                - <a href="?action=edit&id=<?= urlencode($user['UTILISATEURID']) ?>">Modifier</a>
-                - <a href="#" class="delete-btn" data-id="<?= htmlspecialchars($user['UTILISATEURID']) ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?');">Supprimer</a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
->>>>>>> 27b86010fa19950eb779e65c4710626fbc157782
 <?php else : ?>
     <p>Aucun utilisateur trouvé.</p>
 <?php endif; ?>
 
 <a href="?action=create">Ajouter un utilisateur</a>
-<<<<<<< HEAD
-=======
-<script src="../public/js/main.js"></script>
-<link rel="stylesheet" href="../public/css/styles.css">
-</body>
->>>>>>> 27b86010fa19950eb779e65c4710626fbc157782
