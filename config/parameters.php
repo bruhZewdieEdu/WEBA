e77a7b14c94f720ec59881d6b31a3b5eb613a8b4
bruhZewdieEdu <145675370+bruhZewdieEdu@users.noreply.github.com>
2025-01-13 11:21:25 +0100

    Update and rename param.php to parameters.php

---
+++ config/parameters.php
@@ -0,0 +1,12 @@
+<?php
+
+// Paramètres de connexion à la base de données pour WAMP en local
+class Parameters
+{
+    const EDB_DBTYPE = "mysql"; // Type de base de données
+    const EDB_DBNAME = "nom_de_la_base"; // Remplacez par le nom de votre base de données
+    const EDB_HOST = "127.0.0.1"; // Adresse du serveur (localhost ou 127.0.0.1)
+    const EDB_PORT = "3306"; // Port par défaut de MySQL
+    const EDB_USER = "root"; // Nom d'utilisateur par défaut de WAMP
+    const EDB_PASS = ""; // Mot de passe par défaut (vide sur WAMP)
+}
